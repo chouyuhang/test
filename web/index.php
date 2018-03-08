a0320f9ed0feca492d6cd5dd3051ce27130f5342
chouyuhang <a103410102a@gmail.com>
2018-03-08 10:35:39 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,30 +1,29 @@
 <?php
-namespace LINE\Tests\LINEBot;
-use LINE\LINEBot;
-use LINE\Tests\LINEBot\Util\DummyHttpClient;
-class GetProfileTest extends \PHPUnit_Framework_TestCase
-{
-    public function testGetProfile()
-    {
-        $mock = function ($testRunner, $httpMethod, $url, $data) {
-            /** @var \PHPUnit_Framework_TestCase $testRunner */
-            $testRunner->assertEquals('GET', $httpMethod);
-            $testRunner->assertEquals('https://api.line.me/v2/bot/profile/USER_ID', $url);
-            return [
-                'displayName' => 'BOT API',
-                'userId' => 'userId',
-                'pictureUrl' => 'https://example.com/abcdefghijklmn',
-                'statusMessage' => 'Hello, LINE!',
-            ];
-        };
-        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e']);
-        $res = $bot->getProfile('USER_ID');
-        $this->assertEquals(200, $res->getHTTPStatus());
-        $this->assertTrue($res->isSucceeded());
-        $data = $res->getJSONDecodedBody();
-        $this->assertEquals('BOT API', $data['displayName']);
-        $this->assertEquals('userId', $data['userId']);
-        $this->assertEquals('https://example.com/abcdefghijklmn', $data['pictureUrl']);
-        $this->assertEquals('Hello, LINE!', $data['statusMessage']);
+require_once('./LINEBotTiny.php');
+$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
+$channelSecret = getenv('LINE_CHANNEL_SECRET');
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                	$m_message = $message['text'];  $type=$event['type']; 
+                    if($type == $type['text']){
+                        $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => "text"
+                            ))));
+                    }
+                    break;           
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
     }
-}
+};
