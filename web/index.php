073ab68a8ed0353af04b9e32dbec329720026d08
chouyuhang <a103410102a@gmail.com>
2018-03-10 19:56:15 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -3,20 +3,28 @@ require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$bot = new \LINE\LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
+    'channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e'
+]);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                	$m_message = $message['text'];$type= $message['type'];$displayName= $message['displayName'];
+                	$m_message = $message['text'];$type= $message['type'];$displayName1= $message['displayName'];
+                    $res = $bot->getProfile('user-id');
+                    $profile = $res->getJSONDecodedBody();
+                    $displayName = $profile['displayName'];
+                    $statusMessage = $profile['statusMessage'];
+                    $pictureUrl = $profile['pictureUrl'];
                     if($m_message!=""){
                         $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
                                 'type' => 'text',
-                                'text' => $displayName
+                                'text' => $displayName.$displayName1
                             ))));
                     }
                     break;           
