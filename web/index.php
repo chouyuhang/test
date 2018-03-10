5eac102e5049699760cbf643db0c93aa2f2cc4b1
chouyuhang <a103410102a@gmail.com>
2018-03-10 20:25:28 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ use chouyuhang\LINEBot;
 use chouyuhang\LINEBot\HTTPClient\CurlHTTPClient;
 require_once('./LINEBotTiny.php');
 
-$bot = new \src\LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
+$bot = new chouyuhang\src\LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
     'channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e'
 ]);
 foreach ($client->parseEvents() as $event) {
