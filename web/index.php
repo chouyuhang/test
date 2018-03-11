db8ef9e2e062e0914c803b15bbac7cc11fcb2cb7
chouyuhang <a103410102a@gmail.com>
2018-03-11 19:07:16 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,8 @@
 <?php
-namespace chouyuhang\Tests\LINEBot;
-use chouyuhang\LINEBot;
-use chouyuhang\LINEBot\HTTPClient\CurlHTTPClient;
 require_once('./LINEBotTiny.php');
-
-$bot = new LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
-    'channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e']);
+$channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
+$channelSecret = getenv('LINE_CHANNEL_SECRET');
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
