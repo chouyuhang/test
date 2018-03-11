7616dc8417036297572f213334f24a6cb698f4a8
chouyuhang <a103410102a@gmail.com>
2018-03-11 19:24:45 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -10,6 +10,12 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];$type= $message['type'];$displayName1= $message['displayName'];
+                    $url = 'https://api.line.me/v2/bot/profile/{userId}';
+                    $ch = curl_init($url);
+	                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		                'Authorization: Bearer {' . $channel_access_token . '}',
+	                ));
                     if($m_message!=""){
                         $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
