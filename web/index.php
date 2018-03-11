ac6aa714c179d0826af881fefa35037637a02c68
chouyuhang <a103410102a@gmail.com>
2018-03-11 22:07:52 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
 use chouyuhang\src\LINEBot\HTTPClient;
+use chouyuhang\LINEBot;
 require_once('./LINEBotTiny.php'); 
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
@@ -11,7 +12,7 @@ foreach ($client->parseEvents() as $event) {
             switch ($message['type']) {
                 case 'text':
                 	$m_message = $message['text'];$type= $message['type'];$displayName1= $message['displayName'];
-                    $bot = new \LINE\LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
+                    $bot = new \chouyuhang\LINEBot(new CurlHTTPClient('WI8f+ot/+7IJffBJATgfi1+rnNYCW+RGm1u2SRg2sdOLw2Y0+4gbdJsmh0zmUdtZNvx595o+hvI3XYeFQk66EVpl1mWwDDJOlKRecD6mc8gES9hnbAH+SOcrxw3QWmrmvQPI0WxrXMwB8EVOXPx4FwdB04t89/1O/w1cDnyilFU='), [
                     'channelSecret' => 'a7e8c58d4744adbc363c42bc558db89e']);
                     $res = $bot->getProfile('user-id');
 
