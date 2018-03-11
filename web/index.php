8be5c6829ccfc4a94820066b047e0fd18cee5167
chouyuhang <a103410102a@gmail.com>
2018-03-11 21:34:04 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-use chouyuhang\test\web\abc;
+use chouyuhang/test/web/abc.txt;
 require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
