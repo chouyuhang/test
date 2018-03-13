59c9fa36d68d7b7a9820b0a3f3d9f4289d2b5e4e
chouyuhang <a103410102a@gmail.com>
2018-03-13 20:23:24 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+//require __DIR__ . '/../vendor/autoload.php';
 require_once('./LINEBotTiny.php');
 //require_once('/../web/LINEBotTiny.php');
 require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
