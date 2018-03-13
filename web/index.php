7113b8239a12558ce6b903cab1ba7833b0903016
chouyuhang <a103410102a@gmail.com>
2018-03-13 16:22:06 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
-//require_once('./LINEBotTiny.php');
-require_once('/../web/LINEBotTiny.php');
+require_once('./LINEBotTiny.php');
+//require_once('/../web/LINEBotTiny.php');
 require_once __DIR__ . '../src/LINEBot/CurlHTTPClient.php';
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
