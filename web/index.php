6153d1dbe6e9c998a703cccefe5d9fe74dc22a3c
chouyuhang <a103410102a@gmail.com>
2018-03-13 20:56:51 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../src/LINEBot.php';
 require_once __DIR__ . '/../src/LINEBot/Constant/Meta.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient.php';
-require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder';
+require_once __DIR__ . '/../src/LINEBot/MessageBuilder/TextMessageBuilder.php';
 require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
