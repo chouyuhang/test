3ff9758665cc20ca6b1eae81d800008ec753aca9
chouyuhang <a103410102a@gmail.com>
2018-03-13 18:27:24 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../src/LINEBot/HTTPClient/CurlHTTPClient.php';
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-$bot = new HTTPClient(new CurlHTTPClient($channelAccessToken), [
+$bot = new chouyuhang\test\src\LINEBot\HTTPClient(new CurlHTTPClient($channelAccessToken), [
     'channelSecret' => $channelSecret
 ]);
 
